ea00791737fac1f39d20b749d260c7e9dfe40c7f
davlia <danedaworld@gmail.com>
2017-04-18 21:42:48 -0400

    merge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Player from './Player.js'
 import Viewport from './Viewport.js'
 
 const ASSETS = '../assets';
+const SERVER_URL = 'ws://localhost:8000/play';
 
 export default class App {
   constructor () {
@@ -16,11 +17,42 @@ export default class App {
 
     let spriteSrc = `${ASSETS}/sprites.png`;
 
-
+  setup() {
+    this.setupWebsocket();
+    this.setupEventListeners();
   }
 
-  setup() {
-    
+  setupWebsocket() {
+    let ws = new WebSocket(SERVER_URL);
+    ws.onopen = () => {
+      console.log('websocket connection opened');
+      ws.send('ping');
+    }
+    ws.onmessage = (evt) => {
+      console.log(`evt received ${evt}`);
+    }
+    ws.onclose = () => {
+      console.log('websocket connection closed');
+    }
+  }
+
+  setupEventListeners() {
+    this.canvas.addEventListener('keypress', () => {
+      switch (code) {
+        case 37:
+          this.player.move('left');
+          break;
+        case 38:
+          this.player.move('up');
+          break;
+        case 39:
+          this.player.move('right');
+          break;
+        case 40:
+          this.player.move('down');
+          break;
+      }
+    });
   }
 
   onLoad() {
